a09d70d70466906e719ab1dc12b39bfa63454251
SangWooRyu <rookas33@gmail.com>
2023-03-08 21:39:30 +0900

    관리자페이지/쿠폰 ejs화 완료

--- src/views/controllers/management.page.ts
+++ src/views/controllers/management.page.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Param, Render } from '@nestjs/common';
 import { CampService } from 'src/camp/camp.service';
+import { CouponService } from 'src/coupon/coupon.service';
 
 @Controller('view')
 export class ManagementPage {
-  constructor(private readonly campService: CampService) {}
+  constructor(
+    private readonly campService: CampService,
+    private readonly couponService: CouponService,
+  ) {}
 
   @Get('/management')
   @Render('management')
@@ -36,5 +40,25 @@ export class ManagementPage {
   async camp(@Param('id') campId: number) {
     const camp = await this.campService.getCampById(campId);
     return { component: 'camp', camp };
+  }
+
+  @Get('/management/coupon')
+  @Render('management')
+  async coupons() {
+    const coupons = await this.couponService.getCoupons();
+    return { component: 'coupons', coupons };
+  }
+  @Get('/management/coupon/update/:id')
+  @Render('management')
+  async couponUpdateForm(@Param('id') couponId: number) {
+    const coupon = await this.couponService.getCouponById(couponId);
+    return { component: 'couponUpdateForm', coupon };
+  }
+
+  @Get('/management/coupon/:id')
+  @Render('management')
+  async coupon(@Param('id') couponId: number) {
+    const coupon = await this.couponService.getCouponById(couponId);
+    return { component: 'coupon', coupon };
   }
 }
