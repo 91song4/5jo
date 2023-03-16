553c6b1fda151c848765e545a6abc7817d13a62d
zlfmaodtod77 <zlfmaodtod77@naver.com>
2023-03-16 20:20:19 +0900

    reviewAPI

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewController } from './review.controller';
+import { Review } from './review.entity';
 import { ReviewService } from './review.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review])],
   controllers: [ReviewController],
-  providers: [ReviewService]
+  providers: [ReviewService],
+  exports: [ReviewService],
 })
 export class ReviewModule {}
