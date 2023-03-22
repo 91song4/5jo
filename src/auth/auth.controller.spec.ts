86b4fd297499981f9d399bc0d5b0d59fd67b45b9
91song4 <91song4@daum.net>
2023-03-23 03:51:36 +0900

    feat: createUser 테스트코드

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { number } from 'joi';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  // let authService: AuthService;
+
+  const mockAuthService = {
+    createUser: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [AuthService],
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    // a uthService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('createUser()', () => {
+    it('should create a user', async () => {
+      const createUserDto: CreateUserDto = {
+        userId: 'song',
+        password: '123',
+        name: '송지훈',
+        phone: '010-2638-5808',
+        email: 'song4@gmail.com',
+        birthday: new Date('1991-05-24'),
+      };
+
+      mockAuthService.createUser = jest.fn((createUserDto: CreateUserDto) => {
+        return { id: expect.any(number) };
+      });
+
+      const result = await authController.createUser(createUserDto);
+      expect(result).toEqual({
+        id: expect.any(number),
+      });
+    });
   });
 });
